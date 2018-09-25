f85dbee5b4a8a947293cb7ede9bf0ff9a2e7dbc2
Eirik Falck <eirik.falck@gmail.com>
2018-09-25 20:20:15 +0200

    now simulating normal force and curvature. added write_csv function to save data

--- simulering.py
+++ simulering.py
@@ -9,7 +9,7 @@ c = 2/5  # for solid sphere, I_0 / mr^2
 
 
 ## FUNCTIONS
-def first_derivative(f, x, h=0.00001):
+def first_derivative(f, x, h=0.0001):
     """
     Computes first_derivative of function f at point x
     https://en.wikipedia.org/wiki/Numerical_differentiation#Finite_difference_formulas
@@ -17,7 +17,7 @@ def first_derivative(f, x, h=0.00001):
     return (f(x + h) - f(x)) / h
 
 
-def second_derivative(f, x, h=0.00001):
+def second_derivative(f, x, h=0.001):
     """
     https://www.scss.tcd.ie/~dahyotr/CS7ET01/01112007.pdf
     """
@@ -51,12 +51,40 @@ def krumning(f, x):
     return abs(second_derivative(f, x)) / (denom**(3/2))
 
 
+def normal_g_force(v_tangential, krumning):
+    return 1 + v_tangential**2 * krumning / g
+
+
 def bane(x):
-    return 3433.2281869052126*x**15 + -35425.80967983638*x**14 + 164086.51268783736*x**13 + -449939.19577019487*x**12 + 810832.0563168445*x**11 + -1007456.1154303564*x**10 + 880102.071263024*x**9 + -540502.2906810011*x**8 + 228926.39127614404*x**7 + -63888.210540241824*x**6 + 10617.08175653279*x**5 + -759.7060900777494*x**4 + -33.33911458989251*x**3 + 8.555075814607894*x**2 + -1.7645318855075836*x**1 + 0.6532652470202632*x**0
+    return 3433.2281869052126*x**15 + -35425.80967983638*x**14 + \
+            164086.51268783736*x**13 + -449939.19577019487*x**12 + \
+            810832.0563168445*x**11 + -1007456.1154303564*x**10 + \
+            880102.071263024*x**9 + -540502.2906810011*x**8 + \
+            228926.39127614404*x**7 + -63888.210540241824*x**6 + \
+            10617.08175653279*x**5 + -759.7060900777494*x**4 + \
+            -33.33911458989251*x**3 + 8.555075814607894*x**2 + \
+            -1.7645318855075836*x**1 + 0.6532652470202632
+
+
+def write_csv(*args, filename, header, ext="csv"):
+    """
+    *args: lists to be written
+    filename: filename without extension
+    header: list, file header with desc. of which values are where. should 
+            correspond with *args
+    ext: extension without dot
+    """
+    print("Writing to file...")
+    f = open("#", filename + "." + ext, "w")
+    print(",".join(header), file=f)
+    for vals in zip(*args):
+        print(",".join("%.4f" % i for i in vals), file=f)
+    f.close()
+    print("Wrote to", filename + "." + ext)
 
 
 def main():
-    step_h = 0.001  # s
+    step_h = 0.001  # s, eulers method step
     UPPER_T = 1.1  # s, how long to simulate
 
     time_ = [i for i in range(0, int(UPPER_T / step_h))]
@@ -66,12 +94,16 @@ def main():
     pos_y = [0.0] * len(time_)
     speed_tangential = [0.0] * len(time_)
     accel_tangential = [0.0] * len(time_)
+    normal = [0.0] * len(time_)
+    curvature = [0.0] * len(time_)
+    dfdx = [0.0] * len(time_)  # first derivative
+    d2fdx2 = [0.0] * len(time_)  # second derivative
 
     angle[0] = alpha(bane, pos_x[0])
     pos_y[0] = bane(pos_x[0])
 
     # calc points for curve
-    t = [i/100.0 for i in range(0, 140, 1)]
+    t = [i * step_h for i in range(0, int(UPPER_T / step_h))]
     b = list()
     for x in t:
         b.append(bane(x))
@@ -84,13 +116,18 @@ def main():
         pos_x[n+1] = pos_x[n] + step_h * x_comp(speed_tangential[n], angle[n])
         pos_y[n+1] = pos_y[n] - step_h * y_comp(speed_tangential[n], angle[n])
 
+        curvature[n+1] = krumning(bane, pos_x[n])
+        normal[n+1] = normal_g_force(speed_tangential[n], krumning(bane, pos_x[n]))
+
+        dfdx[n] = first_derivative(bane, pos_x[n])
+        d2fdx2[n] = second_derivative(bane, pos_x[n])
+
+
     # write simulation to file
-    f = open("simulation.txt", "w")
-    print("Time\tAngle\tAccel\tSpeed\tX    \tY    ", file=f)
-    for vals in zip(time_, angle, accel_tangential, speed_tangential, pos_x, pos_y):
-        print(str(vals[0]) + "\t" + "\t".join("%.3f" % i for i in vals[1:]), file=f)
-    f.close()
-    print("Wrote to simulation.txt")
+    write_csv(time_, angle, pos_x, pos_y, speed_tangential, accel_tangential,\
+            normal, curvature, dfdx, d2fdx2, filename="simulation",
+            header=["time","angle","x","y","speed_tangential",\
+            "accel_tangential","normal_force","curvature","dfdx","d2fdx2"])
 
     # plotting
     print("Plotting...")
@@ -100,13 +137,28 @@ def main():
     plt.grid()
 
     plt.plot(time_, accel_tangential, "g")
+    plt.plot(time_, curvature, "y")
     plt.plot(time_, speed_tangential, "b")
     plt.plot(time_, pos_x, "c")
     plt.plot(time_, pos_y, "m")
+    plt.plot(time_, normal, "k--")
+    # plt.plot(time_, dfdx, "m")
+    # plt.plot(time_, d2fdx2, "c")
+
     # plt.plot(pos_x, pos_y, 'r--')
     # plt.plot(t, b)  # bane plot
 
-    legends = ("Accel", "Speed", "X", "Y")
+    legends = (
+                "Accel", 
+                "Curvature",
+                "Speed", 
+                "X", 
+                "Y", 
+                "Normal G-force",
+                # "df/dx",
+                # "d^2f/dx^2",
+                ""
+            )
     plt.legend(legends, loc="best")
 
     plt.show()
